2d04cefa6ab5d8222d0a36dad2904284f0552d00
Valentina Ortiz <116057704+svalentinaog@users.noreply.github.com>
2024-01-25 18:12:32 -0500

    update app.ts

--- src/app.ts
+++ src/app.ts
@@ -4,26 +4,27 @@ import { NodemailerService } from "./services/mailer";
 const nodemailerService = new NodemailerService();
 const app = express();
 
+// app.use((req: any, res: any, next: any) => {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+//   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//   next();
+// });
+
 app.use((req: any, res: any, next: any) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  // Handle requests (OPTIONS)
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
   next();
 });
 
 app.use(express.json());
-
-/*   app.get('/', async (req: any res: any) => {
-    const { email } = req.query as { email: string } || { email: '' };
-
-    if (!email) {
-      return res.status(400).json({ error: "El parámetro 'email' es necesario." });
-    }
-
-    const info = await nodemailerService.sendNewUser(email);
-
-    res.(201).json({ info });
-  }); */
 
 app.post("/contact", async (req: any, res: any) => {
   const { email, subject, data } = (req.body as {
@@ -42,19 +43,5 @@ app.post("/contact", async (req: any, res: any) => {
 
   res.status(201).json({ info });
 });
-
-/* 
-app.get('/pay', async (req: Request, res: Response) => {
-  const { email } = req.query as { email: string } || { email: '' };
-
-  if (!email) {
-    return res.status(400).json({ error: "El parámetro 'email' es necesario." });
-  }
-
-  const info = await nodemailerService.payConfirmation(email);
-
-  res.status(201).json({ info });
-});
- */
 
 export default app;
